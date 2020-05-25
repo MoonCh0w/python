ef25655f0220a297c69533c52653d43cfc14ebf4
MoonCh0w <george.gukjr1@gmail.com>
2020-05-24 22:24:42 -0400

    First commit Message

---
+++ magic.py
@@ -0,0 +1,13 @@
+import random
+
+answers_array = ["No.", "Go fuck yourself.", "Yes.", "Maybe.", "Not sure.", "Fuck off.", "Try again later.", "I don't know.", "What do you think?"]
+questions_array = ["What is it?", "What do you want?", "Go ahead, ask your question.", "Yes daddy?"]
+
+que = random.choice(questions_array)
+ans = random.choice(answers_array)
+inp = raw_input(("%s: ") % (que))
+
+if len(inp) >= 1:
+    print(ans)
+else:
+    print("Type something you fucking retard. (Try again)")
